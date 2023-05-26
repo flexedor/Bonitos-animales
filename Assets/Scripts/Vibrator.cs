faadf7a2e24ef14bc8f00f4c82670cac68aaf9bf
Shushar <fedor6574022@gmail.com>
2023-05-26 19:26:31 +0200

    Vibration fix

--- Assets/Scripts/Vibrator.cs
+++ Assets/Scripts/Vibrator.cs
@@ -2,42 +2,41 @@ using UnityEngine;
 
 public static  class Vibrator
 {
-#if UNITY_ANDROID || !UNITY_EDITOR
+#if UNITY_ANDROID && !UNITY_EDITOR
    private static AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-   private static AndroidJavaObject currentActivyty = unityPlayer.GetStatic<AndroidJavaObject>("CurrentActivity");
-   private static AndroidJavaObject vibrator = currentActivyty.Call<AndroidJavaObject>("getSystemService", "vibrator");
+   private static AndroidJavaObject currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+   private static AndroidJavaObject vibrator = currentActivity.Call<AndroidJavaObject>("getSystemService", "vibrator");
 #else
-   private static AndroidJavaClass unityPlayer;
-    private static AndroidJavaObject currentActivyty;
-   private static AndroidJavaObject vibrator;
+    private static AndroidJavaClass unityPlayer;
+    private static AndroidJavaObject currentActivity;
+    private static AndroidJavaObject vibrator;
 #endif
-   public static void Vibrate(long milliseconds = 250)
-   {
-      if (IsAndroid())
-      {
-         vibrator.Call("vibrate", milliseconds);
-      }
-      else
-      {
-         Handheld.Vibrate();
-      }
-   }
+    public static void Vibrate(long milliseconds = 250)
+    {
+        if (IsAndroid())
+        {
+            vibrator.Call("vibrate", milliseconds);
+        }
+        else
+        {
+            Handheld.Vibrate();
+        }
+    }
 
-   public static void Cancel()
-   {
-      if (IsAndroid())
-      {
-         vibrator.Call("cancel");
-      }
-   }
+    public static void Cancel()
+    {
+        if (IsAndroid())
+        {
+            vibrator.Call("cancel");
+        }
+    }
 
-   private static bool IsAndroid()
-   {
-      #if UNITY_ANDROID && !UNITY_EDITOR
+    private static bool IsAndroid()
+    {
+#if UNITY_ANDROID && !UNITY_EDITOR
          return true;
-      #else
-         return false;
-      #endif
-   }
+#else
+        return false;
+#endif
+    }
 }
-
